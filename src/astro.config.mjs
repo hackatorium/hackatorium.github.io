25ab1d34ec89d494842b487a2d5e0f43a7511285
robertsinfosec <490545+robertsinfosec@users.noreply.github.com>
2025-02-23 18:20:44 -0500

    Remove unused files and add new user and task documentation

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -7,9 +7,9 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Hackatorium',
-			logo: { 
-				dark:'./src/assets/LogoSquareForDark.png', 
-				light:'./src/assets/LogoSquareForLight.png', 
+			logo: {
+				dark: './src/assets/LogoSquareForDark.png',
+				light: './src/assets/LogoSquareForLight.png',
 				replacesTitle: true,
 			},
 			favicon: '/favicon.png',
@@ -17,22 +17,48 @@ export default defineConfig({
 				github: 'https://github.com/hackatorium',
 			},
 			sidebar: [
-				// {
-				// 	label: 'Guides',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-				// {
-				// 	label: 'Reference',
-				// 	autogenerate: { directory: 'reference' },
-				// },
-				// {
-				// 	label: 'Tutorials',
-				// 	autogenerate: { directory: 'tutorials' },
-				// },
 				{
 					label: 'CTFs',
 					items: [
 						{ label: 'TryHackMe CTFs', link: 'ctf/tryhackme' },
+						{
+							label: 'Easy',
+							collapsed: true,
+							items: [
+								{ label: 'basicpentestingjt', link: '/ctf/tryhackme/basicpentestingjt' },
+								{ label: 'picklerick', link: '/ctf/tryhackme/picklerick' },
+								{ label: 'rrootme', link: '/ctf/tryhackme/rrootme' },
+								{ label: 'ohsint', link: '/ctf/tryhackme/ohsint' },
+								{ label: 'cowboyhacker', link: '/ctf/tryhackme/cowboyhacker' },
+								{ label: 'crackthehash', link: '/ctf/tryhackme/crackthehash' },
+								{ label: 'inclusion', link: '/ctf/tryhackme/inclusion' },
+								{ label: 'agentsudoctf', link: '/ctf/tryhackme/agentsudoctf' },
+								{ label: 'overpass', link: '/ctf/tryhackme/overpass' },
+								{ label: 'lazyadmin', link: '/ctf/tryhackme/lazyadmin' },
+								{ label: 'ignite', link: '/ctf/tryhackme/ignite' },
+								{ label: 'startup', link: '/ctf/tryhackme/startup' },
+								{ label: 'tomghost', link: '/ctf/tryhackme/tomghost' },
+								{ label: 'chillhack', link: '/ctf/tryhackme/chillhack' },
+								{ label: 'bruteit', link: '/ctf/tryhackme/bruteit' },
+								{ label: 'fowsniff-ctf', link: '/ctf/tryhackme/fowsniff-ctf' }
+							]
+						},
+						{
+							label: 'Medium',
+							collapsed: true,
+							items: [
+								{ label: 'mrrobot', link: '/ctf/tryhackme/mrrobot' },
+								{ label: 'dogcat', link: '/ctf/tryhackme/dogcat' }
+							]
+						},
+						{
+							label: 'Hard',
+							collapsed: true,
+							items: [
+								{ label: 'dailybugle', link: '/ctf/tryhackme/dailybugle' },
+								{ label: 'internal', link: '/ctf/tryhackme/internal' }
+							]
+						}
 					]
 				},
 			],
